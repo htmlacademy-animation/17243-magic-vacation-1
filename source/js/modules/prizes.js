5ddc6aab18414cda9dbd01d6e7707302951f4f42
Andrey Karamnoff <karamnoff@gmail.com>
2021-12-03 17:58:45 +0300

    Animate title for result when 'game' section is active

--- source/js/modules/prizes.js
+++ source/js/modules/prizes.js
@@ -1,12 +1,12 @@
 import {AccentTypographyBuild} from "../utils";
 
 export default () => {
-  const animationPrizesTitle = new AccentTypographyBuild(
-      `.prizes__title`,
-      750,
-      `active`,
-      `transform`
-  );
+  const animationPrizesTitle = new AccentTypographyBuild({
+    elementSelector: `.prizes__title`,
+    timer: 750,
+    classForActivate: `active`,
+    propertiesList: [`transform`],
+  });
 
   setTimeout(() => {
     animationPrizesTitle.runAnimation();
